caf51fbd2213e5e58eee1db8aeea52d6b87fbdf3
JoshCooper417 <joshcooper417@gmail.com>
2013-01-15 13:48:51 -0500

    Initial Commit

---
+++ Calculator/JECViewController.h
@@ -0,0 +1,13 @@
+//
+//  JECViewController.h
+//  Calculator
+//
+//  Created by Joshua Cooper on 1/15/13.
+//  Copyright (c) 2013 Joshua Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JECViewController : UIViewController
+
+@end
---
+++ Calculator/JECViewController.m
@@ -0,0 +1,29 @@
+//
+//  JECViewController.m
+//  Calculator
+//
+//  Created by Joshua Cooper on 1/15/13.
+//  Copyright (c) 2013 Joshua Cooper. All rights reserved.
+//
+
+#import "JECViewController.h"
+
+@interface JECViewController ()
+
+@end
+
+@implementation JECViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
